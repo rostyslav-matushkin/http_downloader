1a05e45112dfb6586e09f1632f2539f70789d318
Rostyslav M <rm.cooperation@gmail.com>
2017-11-09 16:26:51 +0200

    dev

--- src/main/java/com/rmatushkin/Downloader.java
+++ src/main/java/com/rmatushkin/Downloader.java
@@ -7,13 +7,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 
 import static com.rmatushkin.util.FileSystemReader.createDirectory;
 import static com.rmatushkin.util.FileSystemReader.findFile;
 import static com.rmatushkin.util.ProgramArgumentParser.parse;
+import static java.lang.Integer.parseInt;
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static java.util.concurrent.Executors.newWorkStealingPool;
 
 public class Downloader {
     private static final String[] PARAMETERS = {"n", "l", "f", "o"};
@@ -26,10 +33,11 @@ public class Downloader {
 
     public void start() {
         Map<String, String> valuesByParameters = parse(args, PARAMETERS);
+        int threadsQuantity = parseInt(valuesByParameters.get(PARAMETERS[0]));
         File foundFile = findFile(valuesByParameters.get(PARAMETERS[2]));
         File destinationDirectory = createDirectory(valuesByParameters.get(PARAMETERS[3]));
         Map<String, String> fileNamesByUrls = fetchFileNamesByUrls(foundFile);
-        downloadFiles(fileNamesByUrls, destinationDirectory);
+        downloadFiles(fileNamesByUrls, destinationDirectory, threadsQuantity);
     }
 
     private Map<String, String> fetchFileNamesByUrls(File file) {
@@ -46,12 +54,24 @@ public class Downloader {
         }
     }
 
-    private void downloadFiles(Map<String, String> fileNamesByLinks, File destinationDirectory) {
+    private void downloadFiles(Map<String, String> fileNamesByLinks, File destinationDirectory, int threadsQuantity) {
         HttpClient httpClient = new HttpClient();
+        List<Callable<Object>> tasks = new ArrayList<>();
         for (Entry<String, String> pair : fileNamesByLinks.entrySet()) {
             String fileName = pair.getKey();
             String url = pair.getValue();
-            httpClient.downloadByUrl(url, fileName, destinationDirectory);
+            String destinationFilePath = destinationDirectory.getPath() + "\\" + fileName;
+            tasks.add(() -> {
+                httpClient.downloadByUrl(url, destinationFilePath);
+                return null;
+            });
         }
+        ExecutorService executorService = newFixedThreadPool(threadsQuantity);
+        try {
+            executorService.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            throw new DownloaderException(e.getMessage());
+        }
+        executorService.shutdown();
     }
 }
--- src/main/java/com/rmatushkin/http/HttpClient.java
+++ src/main/java/com/rmatushkin/http/HttpClient.java
@@ -3,17 +3,15 @@ package com.rmatushkin.http;
 import com.rmatushkin.exception.HttpClientException;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
 public class HttpClient {
 
-    public void downloadByUrl(String url, String destinationFilePath, File destinationDirectory) {
-        String destinationPath = destinationDirectory.getPath() + "\\" + destinationFilePath;
+    public void downloadByUrl(String url, String destinationFilePath) {
         try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(destinationPath)) {
+             FileOutputStream fileOutputStream = new FileOutputStream(destinationFilePath)) {
             byte[] buffer = new byte[1024];
             int count;
             while ((count = bufferedInputStream.read(buffer)) != -1) {

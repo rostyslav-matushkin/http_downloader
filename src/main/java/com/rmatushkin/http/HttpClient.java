f435b8cfd32869b7134aff9636bff9a494ddb61e
Rostyslav M <rm.cooperation@gmail.com>
2017-11-09 17:01:03 +0200

    dev

--- src/main/java/com/rmatushkin/http/HttpClient.java
+++ src/main/java/com/rmatushkin/http/HttpClient.java
@@ -5,19 +5,14 @@ import com.rmatushkin.exception.HttpClientException;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.net.URL;
 
 import static com.rmatushkin.enums.Unit.MEGABYTE;
-import static java.net.Proxy.Type.HTTP;
 
 public class HttpClient {
 
     public void downloadByUrl(String url, String destinationFilePath) {
-        Proxy proxy = new Proxy(HTTP, new InetSocketAddress("195.230.131.201", 3128));
-        try (BufferedInputStream bufferedInputStream =
-                     new BufferedInputStream(new URL(url).openConnection(proxy).getInputStream());
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(destinationFilePath)) {
             byte[] buffer = new byte[MEGABYTE.getBytes()];
             int count;

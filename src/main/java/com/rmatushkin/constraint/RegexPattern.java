38cc1f066fc3d414217aafa0545a258b1a96797f
Rostyslav M <rm.cooperation@gmail.com>
2017-11-12 01:23:08 +0200

    dev

--- src/main/java/com/rmatushkin/constraint/RegexPattern.java
+++ src/main/java/com/rmatushkin/constraint/RegexPattern.java
@@ -5,4 +5,5 @@ public class RegexPattern {
             "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$";
     public static final String UTF8_BOM = "\uFEFF";
     public static final String MANY_WHITESPACES = "\\s+";
+    public static final String LIMIT_PATTERN = "^(\\d+)([kK]|[mM])$";
 }

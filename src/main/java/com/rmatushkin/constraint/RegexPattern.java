b7be7a5496aaabe2f819e7607bd427eb6478a813
Rostyslav M <rm.cooperation@gmail.com>
2017-11-29 16:51:39 +0200

    dev

--- src/main/java/com/rmatushkin/constraint/RegexPattern.java
+++ src/main/java/com/rmatushkin/constraint/RegexPattern.java
@@ -6,6 +6,6 @@ public class RegexPattern {
     public static final String LIMIT_REGEX = "^(\\d+)([kK]|[mM])$";
     public static final String URL_REGEX = "https?.+";
     public static final String URL_AND_FILE_NAME_REGEX = "^" + URL_REGEX + MANY_WHITESPACES_REGEX + ".+$";
-    public static final String FILE_EXTENSION = "\\..+&";
+    public static final String FILE_EXTENSION = "\\.\\w+$";
 
 }

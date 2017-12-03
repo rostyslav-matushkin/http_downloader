4aa90d825fdeabdbc9619e59c6eb7d3d0415ad37
Rostyslav <rm.cooperation@gmail.com>
2017-12-03 14:32:15 +0200

    dev

---
+++ src/main/java/com/rmatushkin/http/Limit.java
@@ -0,0 +1,41 @@
+package com.rmatushkin.http;
+
+import com.rmatushkin.enums.Unit;
+import com.rmatushkin.exception.LimitParseException;
+
+import static com.rmatushkin.constraint.RegexPattern.LIMIT_REGEX;
+import static com.rmatushkin.enums.Unit.KILOBYTE;
+import static com.rmatushkin.enums.Unit.MEGABYTE;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
+
+public class Limit {
+    private final int value;
+    private final Unit unit;
+
+    public Limit(int value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public static Limit parseLimit(String string) {
+        if (string.matches(LIMIT_REGEX) && string.toLowerCase().endsWith(KILOBYTE.getLetter())) {
+            int limitValue = parseInt(string.substring(0, string.length() - 1));
+            return new Limit(limitValue, KILOBYTE);
+        }
+        if (string.matches(LIMIT_REGEX) && string.toLowerCase().endsWith(MEGABYTE.getLetter())) {
+            int limitValue = parseInt(string.substring(0, string.length() - 1));
+            return new Limit(limitValue, MEGABYTE);
+        }
+        System.err.println(format("String %s can't be parsed!", string));
+        throw new LimitParseException();
+    }
+}
